64e9a3ebe77372974ef883c6f3807b6d7ed5a801
jeonghosuk <hosukjeong@gmail.com>
2013-04-22 10:17:15 +0900

    first commit 0422

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jeonghosuk::Application.config.secret_token = '5017b3eee2e51b761a6553a6311c06dd1a9e13a105eafa86055f3dd46e1f983c826c473d4b24af841279a846e4b591d6e4cadabba80d516238e4352c8632f9ba'
